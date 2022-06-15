5ef1afb02cbbeb76e44d701c180e22e0fca53b2a
Bruno Lopes <eusoubruno7@gmail.com>
2022-06-15 17:19:35 -0300

    add filter by user id in userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,33 @@
-const users = require('../mocks/users')
+const users = require("../mocks/users");
 
 module.exports = {
   listUsers(request, response) {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
+    const { order } = request.query;
+
+    const sortedUsers = users.sort((a, b) => {
+      if (order === "desc") return a.id < b.id ? 1 : -1;
+
+      return a.id > b.id ? 1 : -1;
+    });
+
+    response.writeHead(200, { "Content-Type": "application/json" });
 
     response.end(JSON.stringify(users));
-  }
-}
+  },
+
+  getUserById(request, response) {
+    const { id } = request.params;
+
+    const user = users.find((user) => user.id === Number(id));
+
+    if (!user) {
+      response.writeHead(400, { "Content-Type": "application/json" });
+
+      response.end(JSON.stringify({ error: "User not found!" }));
+    }
+
+    response.writeHead(200, { "Content-Type": "application/json" });
+
+    response.end(JSON.stringify(user));
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
 const http = require('http');
+const url = require('url');
+//maneira de fazer nas versões acima da 11
+const { URL } = require('url');
+
 const routes = require('./routes');
 
 const server = http.createServer((request, response) => {
+  // const parsedUrl = url.parse(request.url, true); // não funciona a partir da versão 11 do node
+  //maneira de fazer nas versões acima da 11
+  const parsedUrl = new URL(`http://localhost:300${request.url}`)
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
 
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  // Bolean(algum valor) tranforma para o resultado boolean daquela variavel
+  // const splitEndpoint = pathname.split('/').filter((routeItem) => Boolean(routeItem));
+  const splitEndpoint = pathname.split('/').filter(Boolean); // filtra da mesma forma que a linha acima
+
+  if(splitEndpoint.length > 1){
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
 
   const route = routes.find((routeObject) => (
-    routeObject.endpoint === request.url && routeObject.method === request.method
+    routeObject.endpoint === pathname && routeObject.method === request.method
   ));
 
   if(route){
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.params = { id };
+
     route.handler(request, response);
   } else {
     response.writeHead(404, { 'Content-Type': 'text/html' });
 
-    response.end(`Cannot ${request.method} ${request.url}`);
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
   }
 });
 
